5e49d7eab4f83d725912a5d71cb006037568d87b
Florin Mazilu <maziluflorin84@outlook.com>
2022-10-02 14:57:35 +0300

    Commit #2

---
+++ src/com/ocp/p2/Test1.java
@@ -0,0 +1,27 @@
+package com.ocp.p2;
+
+public class Test1 {
+    String name;
+
+    public Test1() {
+        this.name = "test" + new Test2().string() + new Test3().string();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+class Test2 {
+    String string() {
+        return " new ";
+    }
+}
+class Test3 {
+    String string() {
+        return "class";
+    }
+}
