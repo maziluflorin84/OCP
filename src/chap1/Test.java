95158a35f4d0b370bd7c76d6588832f9dad0ae1b
maziluflorin84 <maziluflorin84@outlook.com>
2025-03-17 19:08:18 +0200

    Ongoing Chapter 1

--- src/chap1/Test.java
+++ src/chap1/Test.java
@@ -1,14 +1,17 @@
 package chap1;
 
 public class Test {
-    public static void main(String[] args) {
+    public final static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println("No. of arguments : " + args.length);
         Tested tested = new Tested();
         tested.runTest();
     }
 }
 
 class Tested {
-    boolean value;
+    boolean value = true;
     public void runTest() {
         checkValue(this.value);
     }
---
+++ src/chap1/Test1.java
@@ -0,0 +1,10 @@
+package chap1;
+
+import java.util.*;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+    }
+}
