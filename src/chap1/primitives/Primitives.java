0869bb36cd29679f4e520e73c42e39dfa44b02ee
maziluflorin84 <maziluflorin84@outlook.com>
2025-03-18 22:46:48 +0200

    Ongoing Chapter 1 #4

--- src/chap1/primitives/Primitives.java
+++ src/chap1/primitives/Primitives.java
@@ -10,7 +10,8 @@ public class Primitives {
         System.out.println("val3 = " + val3);
         int val4 = 2147483647;
         System.out.println("val4 = " + val4);
-        long val5 = 123L;
+        // long val5 = 3123456789;
+        long val5 = 3123456789L;
         System.out.println("val5 = " + val5);
         float val6 = 123.45f;
         System.out.println("val6 = " + val6);
@@ -19,22 +20,32 @@ public class Primitives {
         char val8 = 'a';
         System.out.println("val8 = " + val8);
         
-        System.out.println(Integer.valueOf("0", 16));
-        System.out.println(Integer.valueOf("1", 16));
-        System.out.println(Integer.valueOf("2", 16));
-        System.out.println(Integer.valueOf("3", 16));
-        System.out.println(Integer.valueOf("4", 16));
-        System.out.println(Integer.valueOf("5", 16));
-        System.out.println(Integer.valueOf("6", 16));
-        System.out.println(Integer.valueOf("7", 16));
-        System.out.println(Integer.valueOf("8", 16));
-        System.out.println(Integer.valueOf("9", 16));
-        System.out.println(Integer.valueOf("A", 16));
-        System.out.println(Integer.valueOf("B", 16));
-        System.out.println(Integer.valueOf("C", 16));
-        System.out.println(Integer.valueOf("D", 16));
-        System.out.println(Integer.valueOf("E", 16));
-        System.out.println(Integer.valueOf("F", 16));
-        // System.out.println(Integer.valueOf("G", 16));
+        System.out.print("0 hex -> " + Integer.valueOf("0", 16) + "; ");
+        System.out.print("1 hex -> " + Integer.valueOf("1", 16) + "; ");
+        System.out.print("2 hex -> " + Integer.valueOf("2", 16) + "; ");
+        System.out.print("3 hex -> " + Integer.valueOf("3", 16) + "; ");
+        System.out.print("4 hex -> " + Integer.valueOf("4", 16) + "; ");
+        System.out.print("5 hex -> " + Integer.valueOf("5", 16) + "; ");
+        System.out.print("6 hex -> " + Integer.valueOf("6", 16) + "; ");
+        System.out.print("7 hex -> " + Integer.valueOf("7", 16) + "; ");
+        System.out.print("8 hex -> " + Integer.valueOf("8", 16) + "; ");
+        System.out.print("9 hex -> " + Integer.valueOf("9", 16) + "; ");
+        System.out.print("a hex -> " + Integer.valueOf("a", 16) + "; ");
+        System.out.print("b hex -> " + Integer.valueOf("b", 16) + "; ");
+        System.out.print("c hex -> " + Integer.valueOf("c", 16) + "; ");
+        System.out.print("d hex -> " + Integer.valueOf("d", 16) + "; ");
+        System.out.print("e hex -> " + Integer.valueOf("e", 16) + "; ");
+        System.out.print("f hex -> " + Integer.valueOf("f", 16) + "; ");
+        
+        System.out.println();
+        
+        int binary1 = 0b01111111111111111111111111111111;
+        long binary2 = 0b011111111111111111111111111111111L;
+        int octal1 = 017777777777;
+        long octal2 = 020000000000L;
+        System.out.println("0b01111111111111111111111111111111 binary -> " + Integer.valueOf(binary1) + "; ");
+        System.out.println("0b011111111111111111111111111111111L binary -> " + Long.valueOf(binary2) + "; ");
+        System.out.println("017777777777 octal -> " + Integer.valueOf(octal1) + "; ");
+        System.out.println("0177777777771 octal -> " + Long.valueOf(octal2) + "; ");
     }
 }
