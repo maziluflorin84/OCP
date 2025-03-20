b4dabb363ee9dcdb2ec14ad5bdc187327f3ef826
maziluflorin84 <maziluflorin84@outlook.com>
2025-03-20 22:51:28 +0200

    Ongoing Chapter 1 - TextBlock #2

--- src/chap1/block/Textblock.java
+++ src/chap1/block/Textblock.java
@@ -1,5 +1,21 @@
 package chap1.block;
 
 public class Textblock {
+    public static void main(String[] args) {
+        prepare();
+    }
     
+    public static void prepare() {
+        String labelled = label("""
+            Java Study Guide
+            For Java 21
+            2024 Edition""", "Jeanne & Scott");
+        System.out.println(labelled);
+    }
+    
+    public static String label(String title, String author) {
+        return """
+               Book:
+               """ + title + " by " + author;
+    }
 }
